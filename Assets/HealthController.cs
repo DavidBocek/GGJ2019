69cea971d2c24a777fd064a7460f8fd3457087ef
Tai Der Hui <leadester@gmail.com>
2019-01-26 15:43:52 -0800

    Health controller, simple test enemy guy, dassssh

---
+++ Assets/HealthController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class HealthController : MonoBehaviour
+{
+    public enum HealthControllerDamageResult
+    {
+        eNoDamage,
+        eDamaged,
+        eDead
+    }
+
+    public int maxHealth;
+
+    private int currHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.AreNotEqual( maxHealth, 0 );
+        currHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void HealthController_HealToFull()
+    {
+        currHealth = maxHealth;
+    }
+
+    public HealthControllerDamageResult HealthController_TakeDamage( int damage )
+    {
+        currHealth -= damage;
+
+        if ( currHealth <= 0 )
+        {
+            gameObject.SendMessage( "OnDeath", damage );
+            return HealthControllerDamageResult.eDead;
+        }
+
+        gameObject.SendMessage( "OnDamage", damage );
+        return HealthControllerDamageResult.eDamaged;
+    }
+}
---
+++ Assets/Test_SimpleEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Test_SimpleEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.AreNotEqual( gameObject.GetComponent<HealthController>(), null );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDamage( int damage )
+    {
+        print( "Took damage: " + damage.ToString() );
+    }
+
+    void OnDeath( int damage )
+    {
+        print( "Dead!" );
+        gameObject.GetComponent<HealthController>().HealthController_HealToFull();
+    }
+}
